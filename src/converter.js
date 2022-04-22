a625324cf9ec1659cadbf5004b4cd6eff0ad41ca
JeremyM45 <jeremyvs1945@gmail.com>
2022-04-22 09:56:54 -0700

    add display for conversionAmount

--- src/converter.js
+++ src/converter.js
@@ -1,9 +1,9 @@
 export class Converter  {
   constructor(amount, currency) {
-    this.amount = amount
-    this.currency = currency
+    this.amount = amount;
+    this.currency = currency;
   }
   convert(){
-    this.convertedAmount = this.amount * this.currency
+    this.convertedAmount = this.amount * this.currency;
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import {addOptions} from  "./UI";
+import { Converter } from "./converter";
+import { ExchangeRate } from "./currency-services";
 
 addOptions();
 $('#get-exchange-rate').on('click', async function()  {
-  // let currancyExchange = await ExchangeRate.getExchangRate();
-  // let exhangeRates = currancyExchange.conversion_rates;
-  // console.log(exhangeRates);
-  // console.log(exhangeRates.AED);
+  let currancyExchange = await ExchangeRate.getExchangRate();
+  let exhangeRates = currancyExchange.conversion_rates;
+  console.log(exhangeRates);
   let usdAmount = $('#amount').val();
-  let currency = $('#currency-select').val();
-  console.log(usdAmount);
-  console.log(currency);
+  let selectCurrency = $('#currency-select').val();
+  let conversion = new Converter(usdAmount, exhangeRates[`${selectCurrency}`]);
+  conversion.convert();
+  $('#results').text(conversion.convertedAmount);
 });
